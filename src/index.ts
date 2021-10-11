b5fb7c894382e0aea7faf6bd16d9aae1939b373a
userlifex <eholguinruiz@gmail.com>
2021-10-11 14:25:38 -0500

    start runing server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hola');
+import express, { NextFunction, Request, Response } from 'express';
+
+const app = express();
+const PORT = 3000;
+const ENVIRONMENT = 'development';
+
+app.use(express.json());
+
+app.use('/', (req: Request, res: Response) => {
+  res.json({ name: 'foo', lastName: 'bar' });
+});
+
+app.listen(PORT, async () => {
+  console.log(`Server listening on ${PORT}`);
+});
