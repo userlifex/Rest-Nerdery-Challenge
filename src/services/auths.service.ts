98c6215e551fd2081b27671c7f02016a308314ff
userlifex <eholguinruiz@gmail.com>
2021-10-15 00:42:06 -0500

    generate jwt

--- src/services/auths.service.ts
+++ src/services/auths.service.ts
@@ -1,12 +1,12 @@
 import bcrypt from 'bcryptjs'
 import createError from 'http-errors'
-import dotenv from 'dotenv' /* load environment variables */
 import LoginDto from '../dtos/auths/req/login.dto'
+import TokenDto from '../dtos/auths/res/token.dto'
+import { generateJWTToken } from '../utils'
 import prisma from './prisma.service'
 
-dotenv.config()
 export default class AuthsService {
-  static async login(input: LoginDto): Promise<boolean> {
+  static async login(input: LoginDto): Promise<TokenDto> {
     const account = await prisma.account.findUnique({
       where: { email: input.email },
       rejectOnNotFound: false,
@@ -22,6 +22,8 @@ export default class AuthsService {
       throw new createError.UnprocessableEntity('invalid credentials')
     }
 
-    return isValid
+    return {
+      token: generateJWTToken(account.id),
+    }
   }
 }
